2d8be61349a5a1e40c5ccbf8d4724a203a7e4b66
Marcus Cvjeticanin <mjovanc@protonmail.com>
2022-03-04 10:27:33 +0100

    Updating files

--- blog/src/main/java/com/mjovanc/blog/repository/BlogCategoryRepository.java
+++
@@ -1,9 +0,0 @@
-package com.mjovanc.blog.repository;
-
-import com.mjovanc.blog.model.BlogCategory;
-import org.springframework.data.jpa.repository.JpaRepository;
-import org.springframework.stereotype.Repository;
-
-@Repository
-public interface BlogCategoryRepository extends JpaRepository<BlogCategory, Long> {
-}
--- blog/src/main/java/com/mjovanc/blog/repository/BlogPostRepository.java
+++
@@ -1,9 +0,0 @@
-package com.mjovanc.blog.repository;
-
-import com.mjovanc.blog.model.BlogPost;
-import org.springframework.data.jpa.repository.JpaRepository;
-import org.springframework.stereotype.Repository;
-
-@Repository
-public interface BlogPostRepository extends JpaRepository<BlogPost, Long> {
-}
